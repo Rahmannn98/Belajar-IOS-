29bc729dfcb5324dce477a142b17f2d6d95d5764
Rahman <saifullohrahman@icloud.com>
2024-05-02 15:10:18 +0700

    Initial Commit

---
+++ Projectku/ProjectkuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectkuApp.swift
+//  Projectku
+//
+//  Created by MacBook Air on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectkuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
